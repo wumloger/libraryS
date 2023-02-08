bdaf0afed470940a9523f1e2b59983b5ac16c868
ray <656482519@qq.com>
2023-02-08 17:34:23 +0800

    Initial commit

---
+++ src/main/java/com/librarys/LibrarySApplication.java
@@ -0,0 +1,13 @@
+package com.librarys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrarySApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibrarySApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/librarys/LibrarySApplicationTests.java
@@ -0,0 +1,13 @@
+package com.librarys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrarySApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
